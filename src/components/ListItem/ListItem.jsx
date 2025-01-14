e81e47cc3e8f48e201898c9aaf5a16fcf4bec119
Rinataeva <rinataeva@gmail.com>
2025-01-14 23:04:19 +0300

    text-decoration-line-through

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,32 +1,36 @@
 /* eslint-disable react/prop-types */
 import { useState, useRef, useEffect } from "react";
-import styles from './listItem.module.css';
+import styles from "./listItem.module.css";
 
 export function ListItem(props) {
   const [isChecked, setIsChecked] = useState(false);
-  const labelRef = useRef(null); 
+  const labelRef = useRef(null);
 
   const handleCheckboxState = () => {
     setIsChecked(!isChecked);
   };
 
-    useEffect(() => {
+  useEffect(() => {
     if (labelRef.current) {
-      labelRef.current.style.textTransform = "uppercase"; 
-      labelRef.current.style.color = "purple"; 
+      labelRef.current.style.textTransform = "uppercase";
+      labelRef.current.style.color = "purple";
     }
-  }, []); 
+  }, []);
 
   return (
     <div className={styles.added_item} {...props}>
-      <input 
-        type="checkbox" 
+      <input
+        type="checkbox"
         className={styles.checkbox}
-        onChange={handleCheckboxState} 
+        onChange={handleCheckboxState}
       />
       <label
-        ref={labelRef} 
-        style={isChecked ? { textDecoration: 'line-through' } : { textDecoration: 'none' }}
+        ref={labelRef}
+        style={
+          isChecked
+            ? { textDecoration: "line-through" }
+            : { textDecoration: "none" }
+        }
         className={styles.label}
       >
         {props.name}
